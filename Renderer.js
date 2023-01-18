40311b21501a2eb0db69a575362faffeb7448cb7
fabiooo4 <fabioirimie@gmail.com>
2023-01-18 22:24:05 +0100

    added renderer

--- Renderer.js
+++ Renderer.js
@@ -5,4 +5,49 @@ class Renderer {
     this.angle = angle;
   }
 
+  render() {
+    for (const c of this.string) {
+      switch (c) {
+        case 'F':
+          line(0, 0, this.lineLength, 0);
+          translate(this.lineLength, 0);
+          break;
+
+        case 'G':
+          translate(this.lineLength, 0);
+          break;
+
+        case '+':
+          rotate(this.angle);
+          break;
+
+        case '-':
+          rotate(-this.angle);
+          break;
+
+        case '[':
+          push();
+          break;
+
+        case ']':
+          pop();
+          break;
+      
+        default:
+          break;
+      }
+    }
+  }
+
+  setLength(length) {
+    this.lineLength = length;
+  }
+
+  scaleLength(scale) {
+    this.lineLength *= scale;
+  }
+
+  setString(newString) {
+    this.string = newString;
+  }
 }
--- sketch.js
+++ sketch.js
@@ -1,25 +1,37 @@
-let axiom = 'A';
 let lsys;
 let ruleset = [];
+let renderer;
 
 function setup() {
-  // createCanvas(400, 400);
-  noCanvas();
-
-  // ruleset[0] = new Rule('A', "ABA");
-  // ruleset[1] = new Rule('B', "BBB");
-  ruleset[0] = new Rule('A', "ABA");
-  ruleset[1] = new Rule('B', "BBB");
-
-  lsys = new LSystem(axiom, ruleset);
-
-  createP(axiom);
+  createCanvas(600, 600);
+  
+  ruleset[0] = new Rule('F', "FF+[+F-F-F]-[-F+F+F]");
+  // ruleset[0] = new Rule('F', "F+F-F-F+F");
+  lsys = new LSystem('F', ruleset);
+  renderer = new Renderer(lsys.getSentence(), height/3, radians(25));
 }
 
 function draw() {
+  background(0);  
+
+  translate(width/2, height);
+  rotate(-PI/2);
+  stroke(255);
+  renderer.render();
+  noLoop();
 }
 
+let counter = 0;
+
 function mousePressed() {
-  lsys.generate();
-  createP(lsys.getSentence());
+  if (counter < 5) {
+    push();
+    lsys.generate();
+    renderer.setString(lsys.getSentence());
+    renderer.scaleLength(0.5);
+    console.log(lsys.getSentence());
+    pop();
+    redraw();
+    counter++;
+  }
 }

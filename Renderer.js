31aee1258f9d5f87cbac938477951aef39b7e78c
fabiooo4 <fabioirimie@gmail.com>
2023-01-26 13:48:27 +0100

    added bush1, 2, 3 fractals, stick fractal, leaf fractal, algae

--- Renderer.js
+++ Renderer.js
@@ -1,5 +1,5 @@
 class Renderer {
-  constructor(string, lineLength, angle, startingX, startingY, initialRotation) {
+  constructor(string, lineLength, angle, startingX, startingY, initialRotation, scale = 0.5, lengthFactor = 1) {
     this.string = string;
     this.lineLength = lineLength;
     this.originalLength = this.lineLength;
@@ -7,6 +7,8 @@ class Renderer {
     this.startingX = startingX;
     this.startingY = startingY;
     this.initialRotation = initialRotation;
+    this.lengthFactor = lengthFactor;
+    this.scale = scale;
   }
 
   render() {
@@ -16,30 +18,46 @@ class Renderer {
     for (const c of this.string) {
       switch (c) {
         case 'F':
+          // Draw forward by lineLength
           line(0, 0, this.lineLength, 0);
           translate(this.lineLength, 0);
           break;
 
         case 'f':
+          // Move forward by lineLength without drawing
           translate(this.lineLength, 0);
           break;
 
         case '+':
+          // Rotate left by angle
           rotate(-this.angle);
           break;
 
         case '-':
+          // Rotate right by angle
           rotate(this.angle);
           break;
 
         case '[':
+          // Save state
           push();
           break;
 
         case ']':
+          // Load state
           pop();
           break;
-      
+
+        case '>':
+          // Multiply the length by lengthFactor
+          this.lineLength *= this.lengthFactor
+          break
+          
+        case '<':
+          // Divide the length by lengthFactor
+          this.lineLength /= this.lengthFactor
+          break
+
         default:
           break;
       }
@@ -50,9 +68,8 @@ class Renderer {
     this.lineLength = length;
   }
 
-  scaleLength(scale) {
-    length
-    this.lineLength *= scale;
+  scaleLength() {
+    this.lineLength *= this.scale;
   }
 
   resetLength() {
--- sketch.js
+++ sketch.js
@@ -13,25 +13,95 @@ function setup() {
   textAlign(CENTER);
   select = createSelect();
   select.position(10, 10);
-  select.option('Tree', 0);
-  select.option('Pyramid', 1);
-  select.option('Bush', 2);
+  select.option('Bush 0', 0);
+  select.option('Bush 1', 1);
+  select.option('Bush 2', 2);
+  select.option('Bush 3', 3);
+  select.option('Pyramid', 4);
+  select.option('Leaf', 5);
+  select.option('Stick', 6);
+  select.option('Algae 1', 7);
   select.changed(selectEvent);
 
-  // Tree fractal
+  // Bush 0 fractal
   rulesets.push(new Array(new Rule('F', "FF+[+F-F-F]-[-F+F+F]")));
   lsystems.push(new LSystem('F', rulesets[0]));
-  renderers.push(new Renderer(lsystems[0].getSentence(), 200, radians(25), width/2, height, -PI/2))
+  renderers.push(new Renderer(lsystems[0].getSentence(), 200, radians(25), width/2, height, -PI/2));
+
+  // Bush 1 fractal
+  rulesets.push(new Array(new Rule('F', "F[+FF][-FF]F[-F][+F]F")));
+  lsystems.push(new LSystem('F', rulesets[1]));
+  renderers.push(new Renderer(lsystems[1].getSentence(), 150, radians(35), width/2, height, -PI/2));
+  
+  // Bush 2 fractal
+  rulesets.push(new Array(
+    new Rule('X', "X[-FFF][+FFF]FX"),
+    new Rule('Y', "YFX[+Y][-Y]")
+  ));
+  lsystems.push(new LSystem('Y', rulesets[2]));
+  renderers.push(new Renderer(lsystems[2].getSentence(), 400, radians(25.7), width/2, height, -PI/2));
+
+  // Bush 3 fractal
+  rulesets.push(new Array(
+    new Rule('V', "[+++W][---W]YV"),
+    new Rule('W', "+X[-W]Z"),
+    new Rule('X', "-W[+X]Z"),
+    new Rule('Y', "YZ"),
+    new Rule('Z', "[-FFF][+FFF]F")
+  ));
+  lsystems.push(new LSystem('VZFFF', rulesets[3]));
+  renderers.push(new Renderer(lsystems[3].getSentence(), 130, radians(20), width/2, height-100, -PI/2, 0.8));
 
   // Pyramid fractal
   rulesets.push(new Array(new Rule('F', "F+F-F-F+F")));
-  lsystems.push(new LSystem('F', rulesets[1]));
-  renderers.push(new Renderer(lsystems[1].getSentence(), 200, PI/2, 0, height, 0))
+  lsystems.push(new LSystem('F', rulesets[4]));
+  renderers.push(new Renderer(lsystems[4].getSentence(), 200, PI/2, 0, height, 0));
 
-  // Bush fractal
-  rulesets.push(new Array(new Rule('F', "F[+FF][-FF]F[-F][+F]F")));
-  lsystems.push(new LSystem('F', rulesets[2]));
-  renderers.push(new Renderer(lsystems[2].getSentence(), 150, radians(35), width/2, height, -PI/2))
+  // Leaf fractal
+  rulesets.push(new Array(
+    new Rule('F', ">F<"),
+    new Rule('a', "F[+x]Fb"),
+    new Rule('b', "F[-y]Fa"),
+    new Rule('x', "a"),
+    new Rule('y', "b")
+  ));
+  lsystems.push(new LSystem('a', rulesets[5]));
+  renderers.push(new Renderer(lsystems[5].getSentence(), 4, PI/4, width/2, height, -PI/2, 1, 1.36));
+
+  // Stick fractal
+  rulesets.push(new Array(
+    new Rule('F', "FF"),
+    new Rule('X', "F[+X]F[-X]+X")
+  ));
+  lsystems.push(new LSystem('X', rulesets[6]));
+  renderers.push(new Renderer(lsystems[6].getSentence(), 300, radians(20), width/2, height, -PI/2));
+
+    // Stick fractal
+    rulesets.push(new Array(
+      new Rule('a', "FFFFFv[+++h][---q]fb"),
+      new Rule('b', "FFFFFv[+++h][---q]fc"),
+      new Rule('c', "FFFFFv[+++fa]fd"),
+      new Rule('d', "FFFFFv[+++h][---q]fe"),
+      new Rule('e', "FFFFFv[+++h][---q]fg"),
+      new Rule('g', "FFFFFv[---fa]fa"),
+      new Rule('h', "ifFF"),
+      new Rule('i', "fFFF[--m]j"),
+      new Rule('j', "fFFF[--n]k"),
+      new Rule('k', "fFFF[--o]l"),
+      new Rule('l', "fFFF[--p]"),
+      new Rule('m', "fFn"),
+      new Rule('n', "fFo"),
+      new Rule('o', "fFp"),
+      new Rule('p', "fF"),
+      new Rule('q', "rfF"),
+      new Rule('r', "fFFF[++m]s"),
+      new Rule('s', "fFFF[++n]t"),
+      new Rule('t', "fFFF[++o]u"),
+      new Rule('u', "fFFF[++p]"),
+      new Rule('v', "Fv"),
+    ));
+    lsystems.push(new LSystem('aF', rulesets[7]));
+    renderers.push(new Renderer(lsystems[7].getSentence(), 5, radians(12), width/2, height, -PI/2, 1));
 }
 
 function draw() {
@@ -69,15 +139,14 @@ function keyPressed() {
 }
 
 function generateFractal(fractal) {
-  if (lsystems[fractal].getGeneration() < 5) {
+  if (lsystems[fractal].getGeneration() < 100) {
     push();
     lsystems[fractal].generate();
     renderers[fractal].setString(lsystems[fractal].getSentence());
-    renderers[fractal].scaleLength(0.5);
+    renderers[fractal].scaleLength();
     pop();
 
     generation = lsystems[fractal].getGeneration();
-    console.log(generation);
     redraw();
   }
 }
